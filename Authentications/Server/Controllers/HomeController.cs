2202e18a37e2f5cb05225c28607c0f9d216af461
oleksii.shevtsov <oleksii.shevtsov@nordstrom.com>
2021-10-26 15:59:25 +0300

    [oauth.Server] (Примеры с JwtBearer) [Ep 5]

---
+++ Authentications/Server/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IAuthorizationService _authorizationService;
+
+        public HomeController(IAuthorizationService authorizationService)
+        {
+            _authorizationService = authorizationService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub,"sume_id"),//Ид юзера
+                new Claim("granny","cookie"),
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+            var key = new SymmetricSecurityKey(secretBytes);
+            //var key = new AsymmetricSecurityKey();
+
+
+            var algoritm = SecurityAlgorithms.HmacSha256;
+
+            var signingCredentials = new SigningCredentials(key, algoritm);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer,
+                Constants.Audiance,
+                claims,
+                notBefore: System.DateTime.Now,
+                expires: System.DateTime.Now.AddHours(1),
+                signingCredentials
+                );
+
+            var tokenJson = new JwtSecurityTokenHandler().WriteToken(token);
+            return Ok(new { access_token = tokenJson });
+        }
+
+        public IActionResult Decode(string part)
+        {
+            var bytes = Convert.FromBase64String(part);
+            return Ok(Encoding.UTF8.GetString(bytes));
+        }
+    }
+}
---
+++ Authentications/Server/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication("OAuth")
+                    .AddJwtBearer("OAuth", config =>
+                    {
+                        var secretBytes = Encoding.UTF8.GetBytes(Constants.Secret);
+                        var key = new SymmetricSecurityKey(secretBytes);
+                        //Для передачи в Url
+                        config.Events = new JwtBearerEvents()
+                        {
+                            OnMessageReceived = context =>
+                            {
+                                if (context.Request.Query.ContainsKey("access_token"))
+                                {
+                                    context.Token = context.Request.Query["access_token"];
+                                }
+                                    return Task.CompletedTask;
+                            }
+                        };
+
+
+                        config.TokenValidationParameters = new TokenValidationParameters()
+                        {
+                            IssuerSigningKey = key,
+                            ValidIssuer = Constants.Issuer,
+                            ValidAudience = Constants.Audiance,
+                        };
+                    });
+
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            // Кто ты?
+            app.UseAuthentication();
+            // А у тебя есть доступ?
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+}
